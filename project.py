6b76d1969afc7f4df32fefc811704e9f5eb6943a
EdwardSoto21 <igrofob2001@gmail.com>
2024-02-04 10:49:18 +0100

    U.S Wprowadzanie danych faktury - Format change

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import json
 # Wprowadzenie użytkownikiem danych faktur
 def wprowadz_dane_faktury():
     kwota = float(input("Podaj kwotę faktury: "))
-    data_faktury = input("Podaj datę faktury (w formacie YYYY-MM-DD): ")
+    data_faktury = input("Podaj datę faktury (RRRR-MM-DD): ")
     waluta = input("Podaj walutę: ")
 
     return kwota, data_faktury, waluta
