e3cb4b02224dcdbb4eb85a719d6fa4446ac35438
A Kasarin <>
2024-02-04 11:53:14 +0100

    Add main function to execute app logic

--- project.py
+++ project.py
@@ -1,13 +1,5 @@
 import json
 
-# Wprowadzenie użytkownikiem danych platnosci faktury
-def wprowadz_dane_platnosci():
-    kwota = float(input("Podaj kwotę płatności: "))
-    data_platnosci = input("Podaj datę płatności (RRRR-MM-DD): ")
-    waluta = input("Podaj walutę płatności: ")
-
-    return kwota, data_platnosci, waluta
-
 # Wprowadzenie użytkownikiem danych faktur
 def wprowadz_dane_faktury():
     kwota = float(input("Podaj kwotę faktury: "))
@@ -15,6 +7,14 @@ def wprowadz_dane_faktury():
     waluta = input("Podaj walutę: ")
 
     return kwota, data_faktury, waluta
+
+# Wprowadzenie użytkownikiem danych platnosci faktury
+def wprowadz_dane_platnosci():
+    kwota = float(input("Podaj kwotę płatności: "))
+    data_platnosci = input("Podaj datę płatności (RRRR-MM-DD): ")
+    waluta = input("Podaj walutę płatności: ")
+
+    return kwota, data_platnosci, waluta
 
 # Zapis danych faktur w plik faktury.txt
 def zapisz_faktury(kwota, data, waluta):
@@ -24,7 +24,7 @@ def zapisz_faktury(kwota, data, waluta):
         "Waluta": waluta
     }
     
-    with open("faktury.txt", "a") as plik:
+    with open("dane/faktury.txt", "a") as plik:
         json.dump(faktura, plik)
         plik.write('\n')
 
@@ -36,6 +36,18 @@ def zapisz_platnosc(kwota, data, waluta):
         "Waluta": waluta
     }
     
-    with open("platnosci.txt", "a") as plik:
+    with open("dane/platnosci.txt", "a") as plik:
         json.dump(faktura, plik)
         plik.write('\n')
+
+def main():
+    kwota_faktury, data_faktury, waluta_faktury = wprowadz_dane_faktury()
+    kwota_platnosci, data_platnosci, waluta_platnosci = wprowadz_dane_platnosci()
+    # validate data
+    zapisz_faktury(kwota_faktury, data_faktury, waluta_faktury)
+    zapisz_platnosc(kwota_platnosci, data_platnosci, waluta_platnosci)
+    print(kwota_faktury, data_faktury, waluta_faktury)
+    print(kwota_platnosci, data_platnosci, waluta_platnosci)
+        
+if __name__ == "__main__":
+    main()
