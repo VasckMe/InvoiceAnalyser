ecc1c943d05104d759548524acd2821c42ee950e
A Kasarin <>
2024-02-09 15:26:13 +0100

    U.S - Podsumowanie płatności - add understanding message about the result

--- functions/calculate_data.py
+++ functions/calculate_data.py
@@ -1,9 +1,12 @@
 # Oblicz roznice kwoty faktury i kwoty platnosci
 def oblicz_roznice(kwota_faktury, kwota_platnosci):
-    roznica = abs(kwota_faktury - kwota_platnosci)
+    roznica = round(abs(kwota_faktury - kwota_platnosci), 2)
     if roznica < 0.001:
+        print("Faktura zostala oplacona")
         return "Oplacona", roznica
     elif kwota_faktury < kwota_platnosci:
-        return "Za duzo oplacona", roznica
+        print("Faktura zostala oplacona, ostatek na koncie: ",roznica)
+        return "Oplacona z ostatkiem", roznica
     else:
-        return "Nie oplacona", roznica
+        print("Faktora nie zostala oplacona, brakuje: ",roznica)
+        return "Nie oplacona, brakuje", roznica
--- project.py
+++ project.py
@@ -35,22 +35,25 @@ def reload():
         return
 
 def main():
+    
+    # Wprowadzenie danych
     kwota_faktury, data_faktury, kod_waluty_faktury = wprowadz_dane_faktury()
     kwota_platnosci, data_platnosci, kod_waluty_platnosci = wprowadz_dane_platnosci()
 
+    # Zapis danych do pliku
     zapisz_faktury_do_pliku(kwota_faktury, data_faktury, kod_waluty_faktury)
     zapisz_platnosc_do_pliku(kwota_platnosci, data_platnosci, kod_waluty_platnosci)
 
+    # Pobieranie kursu walut
     kurs_waluty_faktury = pobierz_kurs_waluty(kod_waluty_faktury, data_faktury)
     kurs_waluty_platnosci = pobierz_kurs_waluty(kod_waluty_platnosci, data_platnosci)
 
+    # Sprawdzenie, czy pobrali sie kursy walut, obliczanie roznicy i zapis do pliku
     if kurs_waluty_faktury is not None and kurs_waluty_platnosci is not None:
         wynik, roznica = oblicz_roznice(kwota_faktury*kurs_waluty_faktury, kwota_platnosci*kurs_waluty_platnosci)
-        print(wynik)
         zapisz_wynik_platnosci_do_pliku(wynik, roznica)
-    else:
-        print("Error appeared, kurs waluty faktury or kurs waluty platnosci is None")
 
+    # Uruchomic program ponownie
     spoboj_ponownie("Chcesz kontynuowac ponownie? TAK/NIE", main)
 
 if __name__ == "__main__":
